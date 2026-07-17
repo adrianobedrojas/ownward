2c0856ecc6b197983a91fcb402e0b986b031b77a
adrianobedrojas <adrianrojasx2@gmail.com>
2026-07-17 17:36:05 -0500

    Create upload page
    Adds the document upload page where users will be able to submit files to their LifeVault account.

---
+++ app/upload/page.tsx
@@ -0,0 +1,13 @@
+export default function UploadPage() {
+  return (
+    <main>
+      <h1>Upload Document</h1>
+
+      <input type="file" />
+
+      <button>
+        Upload
+      </button>
+    </main>
+  );
+}
