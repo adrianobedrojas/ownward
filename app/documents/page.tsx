b353616acf956caa880160415a64c45cd54ff420
adrianobedrojas <adrianrojasx2@gmail.com>
2026-07-17 17:37:15 -0500

    Create documents page
    Adds the initial documents page where users can browse and manage files stored in their LifeVault account.

---
+++ app/documents/page.tsx
@@ -0,0 +1,13 @@
+export default function DocumentsPage() {
+  return (
+    <main>
+      <h1>My Documents</h1>
+
+      <ul>
+        <li>Passport.pdf</li>
+        <li>Insurance.pdf</li>
+        <li>TaxReturn.pdf</li>
+      </ul>
+    </main>
+  );
+}
