e7c4a8230944b8f1b27ebd5b8c82e5c09277daee
adrianobedrojas <adrianrojasx2@gmail.com>
2026-07-28 06:26:08 +0000

    Refactor marketplace route to /buy and update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -93,7 +93,7 @@ export default function Navbar() {
           </Link>
         </div>
 
-        <details className="relative lg:hidden">
+        <details className="relative lg:hidden" suppressHydrationWarning>
           <summary className="cursor-pointer rounded-lg border border-slate-700 bg-slate-900 px-4 py-2 text-sm font-semibold text-white">
             Menu
           </summary>
